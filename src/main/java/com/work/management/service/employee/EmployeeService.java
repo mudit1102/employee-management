b715cb1f6ff71162ebb6d5d1429619009d6042c7
Mudit <mudit@Mudits-MacBook-Air.local>
2019-10-01 00:25:38 +0530

    Get Employee By UserName

--- src/main/java/com/work/management/service/employee/EmployeeService.java
+++ src/main/java/com/work/management/service/employee/EmployeeService.java
@@ -1,6 +1,10 @@
 package com.work.management.service.employee;
 
+import com.work.management.dto.EmployeeDto;
+
 public interface EmployeeService {
 
   void save(com.work.management.dto.EmployeeDto document);
+
+  EmployeeDto getEmployeeByUserName(String username);
 }
