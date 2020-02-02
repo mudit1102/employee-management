c35bfafa21af3db12ba039f77d5e277a60bcb6e2
Mudit <mudit@Mudits-MacBook-Air.local>
2020-02-02 17:37:33 +0530

    Enable Application as Eureka Client

--- src/main/java/com/work/management/RestWorkmanagementApplication.java
+++ src/main/java/com/work/management/RestWorkmanagementApplication.java
@@ -2,8 +2,12 @@ package com.work.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@RefreshScope
 @SpringBootApplication
+@EnableEurekaClient
 public class RestWorkmanagementApplication {
 
   public static void main(String[] args) {
